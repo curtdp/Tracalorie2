981e69c76409c49e6d8782eb56f06dc00341034c
Igor Oliinyk <igor.oleynikdp@gmail.com>
2019-03-13 12:18:19 +0200

    Добавляем пересчет калорий при обновлении элемента Выходим из состояния редактирования

--- app.js
+++ app.js
@@ -281,6 +281,13 @@ const App = (function(ItemCtrl, UICtrl) {
     // Update UI
 
     UICtrl.updateListItem(updatedItem);
+
+    // Get total calories
+    const totalCalories = ItemCtrl.getTotalCalories();
+    // Add total calories to UI
+    UICtrl.showTotalCalories(totalCalories);
+
+    UICtrl.clearEditState();
     e.preventDefault();
   };
 
